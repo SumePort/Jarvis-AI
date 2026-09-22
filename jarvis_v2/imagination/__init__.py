47ad46da166d8dc4c99f4d739e24b235b18b3bf5
SumePort <sumeport@gmail.com>
2026-09-22 18:52:51 +0530

    Add imagination and experimental reasoning layer

---
+++ jarvis_v2/imagination/__init__.py
@@ -0,0 +1,5 @@
+from .engine import ImaginationEngine
+from .models import (
+    PossibilityLevel, Hypothesis, Analogy, Experiment, ImaginationResult,
+)
+__all__ = ["ImaginationEngine","PossibilityLevel","Hypothesis","Analogy","Experiment","ImaginationResult"]
