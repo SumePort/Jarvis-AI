06a0a5352f470b3b214d10dbb94a772fb3f61f41
SumePort <sumeport@gmail.com>
2026-09-22 15:34:21 +0530

    feat: add unified knowledge graph fusion

---
+++ jarvis_v2/knowledge/__init__.py
@@ -0,0 +1,4 @@
+from .world_graph import UnifiedWorldGraph, WorldNode, WorldEdge
+from .fusion import KnowledgeGraphFusion
+
+__all__ = ["UnifiedWorldGraph", "WorldNode", "WorldEdge", "KnowledgeGraphFusion"]
