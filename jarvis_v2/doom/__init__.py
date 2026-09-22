4542b117d1410df3d90c590e9a6551e56abb7274
SumePort <sumeport@gmail.com>
2026-09-22 17:26:55 +0530

    feat: export DOOM identity primitives

--- jarvis_v2/doom/__init__.py
+++ jarvis_v2/doom/__init__.py
@@ -1,3 +1,8 @@
 """DOOM resource routing bridge for JARVIS V2."""
 from .router import DoomResourceRouter, RoutedTask
 __all__ = ["DoomResourceRouter", "RoutedTask"]
+
+from .identity import DoomIdentityBridge, DoomIdentitySession
+from .identity_device import IdentityDeviceBinder, DeviceIdentityContext
+
+__all__ = ["DoomIdentityBridge", "DoomIdentitySession", "IdentityDeviceBinder", "DeviceIdentityContext"]
