e54da4937cf1e09ae0c233827011171d27c43f56
SumePort <sumeport@gmail.com>
2026-09-22 13:05:14 +0530

    feat: add JARVIS local tool adapters

---
+++ jarvis_v2/tools/registry.py
@@ -0,0 +1,19 @@
+"""Tool registry bridging abstract plans to concrete handlers."""
+from __future__ import annotations
+from jarvis_v2.actions.executor import ActionExecutor
+from jarvis_v2.core.types import ToolSpec
+
+class ToolRegistry:
+    def __init__(self) -> None:
+        self.specs: dict[str, ToolSpec] = {}
+        self.handlers = {}
+
+    def register(self, spec: ToolSpec, handler) -> None:
+        self.specs[spec.name] = spec
+        self.handlers[spec.name] = handler
+
+    def executor(self) -> ActionExecutor:
+        return ActionExecutor(dict(self.handlers))
+
+    def specs_list(self) -> list[ToolSpec]:
+        return list(self.specs.values())
