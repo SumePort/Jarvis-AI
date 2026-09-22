fd8fb02b91457551a3dbce02f5458ce051068061
SumePort <sumeport@gmail.com>
2026-09-22 13:23:29 +0530

    feat: add JARVIS model gateway

---
+++ jarvis_v2/models/__init__.py
@@ -0,0 +1,3 @@
+"""Model gateway and provider selection for JARVIS V2."""
+from .gateway import ModelGateway, ModelProfile, ModelSelection
+__all__ = ["ModelGateway", "ModelProfile", "ModelSelection"]
